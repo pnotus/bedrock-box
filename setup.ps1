06330959f7cc9dfaa4d07900834f423fb9c0c0f0
Pontus Frendin <pontus@frend.in>
2021-03-17 23:02:18 +0100

    Script to run winget

---
+++ setup.ps1
@@ -0,0 +1,18 @@
+if ((winget source list | Select-String -Pattern msstore).Matches.Length -eq 0) {
+  Write-Warning "msstore saknas som source"
+  exit
+}
+winget install Microsoft.WindowsTerminal --source msstore
+winget install Microsoft.PowerShell
+winget install Mozilla.Firefox
+winget install 7zip.7zip
+winget install Anaconda.Anaconda3
+winget install Audacity.Audacity
+winget install ElectronicArts.Origin
+winget install Git.Git
+winget install Inkscape.Inkscape
+winget install KDE.KDiff3
+winget install Microsoft.VisualStudioCode-User-x64
+winget install VideoLAN.VLC
+winget install Valve.Steam
+winget install GOG.Galaxy
